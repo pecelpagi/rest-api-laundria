3d5466aefd5ec61ac4d1113bd0df8a83e0bc7fb6
Galuh Ramadhan <galuh.html@gmail.com>
2024-02-03 19:59:12 +0700

    test: laundry package model - get all data

--- application/models/Laundry_package_model.php
+++ application/models/Laundry_package_model.php
@@ -22,9 +22,13 @@ class Laundry_package_model extends CI_model {
         return $query->row(0);
     }
 
-    public function get_all_data($limit = NULL, $offset = 0, $search) {
-        if ($search) { $this->db->where("LOWER(name) LIKE LOWER('%{$search}%')"); }
-        if ($limit) { $this->db->limit($limit); }
+    public function get_all_data($limit = NULL, $offset = 0, $search = NULL) {
+        if ($search) {
+            $this->db->where("LOWER(name) LIKE LOWER('%{$search}%')");
+        }
+        if ($limit) {
+            $this->db->limit($limit);
+        }
     
         $this->db->offset($offset);
 
---
+++ application/tests/models/Laundry_package_model_test.php
@@ -0,0 +1,29 @@
+<?php
+
+class Laundry_package_model_test extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->resetInstance();
+        $this->CI->load->model('laundry_package_model', 'laundry_package');
+    }
+
+    public function test_get_all_data()
+    {
+        $output = $this->CI->laundry_package->get_all_data();
+        $this->assertEquals(count($output), 2);
+
+        $limit = 5;
+        $output = $this->CI->laundry_package->get_all_data($limit);
+        $this->assertEquals(count($output), 2);
+        
+        $search = 'cuci kering';
+        $offset = 0;
+        $output = $this->CI->laundry_package->get_all_data($limit, $offset, $search);
+        $this->assertEquals(count($output), 1);
+        
+        $offset = 1;
+        $output = $this->CI->laundry_package->get_all_data($limit, $offset, $search);
+        $this->assertEquals(count($output), 0);
+    }
+}
